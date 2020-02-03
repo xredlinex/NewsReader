9d8ae696e91504c4550496d676988036937e44e7
xredlinex <redik.line@gmail.com>
2020-02-04 00:33:12 +0200

    make model

--- NewsReader/Model/NewsModel.swift
+++ NewsReader/Model/NewsModel.swift
@@ -7,3 +7,60 @@
 //
 
 import Foundation
+import UIKit
+import ObjectMapper
+
+class NewsModel: Mappable {
+    
+    var status: String?
+    var totalResults: Int?
+    var articles: [NewsArticlesSourseModel]?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        status          <- map["status"]
+        totalResults    <- map["totalResults"]
+        articles        <- map["author"]
+    }
+}
+
+class NewsArticlesModel: Mappable {
+    var sourse: NewsArticlesSourseModel?
+    var author: String?
+    var title: String?
+    var description: String?
+    var url: String?
+    var urlToImage: String?
+    var publishAt: String?
+    var content: String?
+    var favoriteNews: Bool?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        sourse          <- map["sourse"]
+        author          <- map["author"]
+        title           <- map["title"]
+        description     <- map["description"]
+        url             <- map["url"]
+        urlToImage      <- map["urlToImage"]
+        publishAt       <- map["publishAt"]
+        content         <- map["content"]
+    }
+}
+
+class NewsArticlesSourseModel: Mappable {
+    var id: String?
+    var name: String?
+    
+    required init?(map: Map) {
+    }
+    
+    func mapping(map: Map) {
+        id      <- map["id"]
+        name    <- map["name"]
+    }
+}
