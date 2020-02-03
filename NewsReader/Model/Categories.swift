2fb89d2b4d3159fdcf1427051c8eba3e10109964
xredlinex <redik.line@gmail.com>
2020-02-04 00:49:42 +0200

    make categoriesl

---
+++ NewsReader/Model/Categories.swift
@@ -0,0 +1,43 @@
+//
+//  Categories.swift
+//  NewsReader
+//
+//  Created by alexey sorochan on 04.02.2020.
+//  Copyright © 2020 alexey sorochan. All rights reserved.
+//
+
+import UIKit
+
+class NewsCategories {
+    var name: String?
+    var keyword: String?
+}
+
+class MakeNewsCategories {
+    func makeCat() -> [NewsCategories]{
+        let sport = NewsCategories()
+        sport.name = "Sport"
+        
+        let political = NewsCategories()
+        political.name = "Political"
+        
+        let humman = NewsCategories()
+        humman.name = "Humman"
+        
+        let health = NewsCategories()
+        health.name = "Health"
+        
+        let technique = NewsCategories()
+        technique.name = "Technique"
+        
+        let weather = NewsCategories()
+        weather.name = "Weather"
+        
+        let crime = NewsCategories()
+        crime.name = "Crime"
+        
+        return [sport, political, humman, health, technique, weather, crime]
+    }
+}
+
+
