7aedc33c895a908c5a6e3f7e55b321c3f6587037
xredlinex <redik.line@gmail.com>
2020-02-04 12:10:57 +0200

    done categories

--- NewsReader/Model/Categories.swift
+++ NewsReader/Model/Categories.swift
@@ -17,24 +17,31 @@ class MakeNewsCategories {
     func makeCat() -> [NewsCategories]{
         let sport = NewsCategories()
         sport.name = "Sport"
+        sport.keyword = "sport"
         
         let political = NewsCategories()
         political.name = "Political"
+        political.keyword = "political"
         
         let humman = NewsCategories()
         humman.name = "Humman"
+        humman.keyword = "humman"
         
         let health = NewsCategories()
         health.name = "Health"
+        health.keyword = "health"
         
         let technique = NewsCategories()
         technique.name = "Technique"
+        technique.keyword = "tech"
         
         let weather = NewsCategories()
         weather.name = "Weather"
+        weather.keyword = "weather"
         
         let crime = NewsCategories()
         crime.name = "Crime"
+        crime.keyword = "crime"
         
         return [sport, political, humman, health, technique, weather, crime]
     }
